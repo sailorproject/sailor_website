84ad1d38803f390ba2cbc1a0cb162920cc80a0fe
Etiene <dalcol@etiene.net>
2014-03-19 00:49:44 +0000

    More documentation!

--- controllers/docs.lua
+++ controllers/docs.lua
@@ -4,5 +4,12 @@ function docs.index(page)
 	page.layout = "legend/inside"
 	page:render('index',{show_brand=true})
 end
-
+function docs.tutorial(page)
+	page.layout = "legend/inside"
+	page:render('tutorial',{show_brand=true})
+end
+function docs.manual(page)
+	page.layout = "legend/inside"
+	page:render('manual',{show_brand=true})
+end
 return docs
